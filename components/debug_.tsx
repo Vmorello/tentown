6998fed0cdafe6477d9eb8ec587f25d08fc360dc
vmorello <victorio.morello@gmail.com>
2024-08-29 17:01:35 -0400

    more phone debugging

--- components/debug_.tsx
+++ components/debug_.tsx
@@ -1,9 +1,21 @@
+"use client"
+
 export function Debug(props:{info:string}){
+
     return(
 
         <div className={"fixed bottom-10 right-10 opacity-75 bg-slate-400"}>
-                        <label>width: </label>
-                        <label id={"info"}>{props.info}</label>
+            <div> 
+                <label>width: </label>
+                <label id={"info"}>{props.info}</label>
+            </div>
+            <div> 
+                <label>window width: </label>
+                <label id={"info"}>{window.innerWidth}</label>
+            </div>
+                       
+                       
+
         </div>
     )
 }
