868bec1924680698671d52327b8f7e4c8b2db3f2
vmorello <victorio.morello@gmail.com>
2024-08-29 17:10:42 -0400

    can be this inconsistancy

--- components/wrappers/aligner.tsx
+++ components/wrappers/aligner.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 
 export default function Aligner({children, canvasWidth}: {children: React.ReactNode,canvasWidth:number}) {
 
-    const [isWindowSmall4Image, setIsWindowSmall4Image] = useState("relative")
+    const [isWindowSmall4Image, setIsWindowSmall4Image] = useState("")
 
     useEffect(() => {
         console.log(`comparing window ${window.innerWidth} to prop ${canvasWidth}`)
