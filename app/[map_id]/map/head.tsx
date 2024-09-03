a0fc0bf3a96d1cd0dbef70078b572faf00a80b46
vmorello <victorio.morello@gmail.com>
2024-09-03 10:55:11 -0400

    updating what is seen in the tab bar

--- app/[map_id]/map/head.tsx
+++ app/[map_id]/map/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-        <title>Ten Towns</title>
+        {/* <title>Ten Towns</title>
         <meta name="description" content="It's Ten Town!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" /> */}
     </>
   )
 }
--- app/wheel/head.tsx
+++ app/wheel/head.tsx
@@ -1,10 +1,11 @@
 export default function Head() {
   return (
     <>
+        {/* - this isnt being displayed -
         <title>Fortune's Wheel</title>
         <meta name="description" content="The grand show!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" /> */}
     </>
   )
 }
