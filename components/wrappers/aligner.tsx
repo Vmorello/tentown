ab1beabb58ffd921d6df0dde7b03c696f8b30366
vmorello <victorio.morello@gmail.com>
2024-09-25 19:25:33 -0400

    added all the emojis ........

--- components/wrappers/aligner.tsx
+++ components/wrappers/aligner.tsx
@@ -2,16 +2,16 @@
 
 import { useEffect, useState } from "react"
 
-export default function Aligner({children, canvasWidth}: {children: React.ReactNode,canvasWidth:number}) {
+export default function Aligner({ children, canvasWidth }: { children: React.ReactNode, canvasWidth: number }) {
 
     const [isWindowSmall4Image, setIsWindowSmall4Image] = useState("relative")
 
     useEffect(() => {
         console.log(`comparing window ${window.outerWidth} to prop ${canvasWidth}`)
         setIsWindowSmall4Image(
-            window.outerWidth < canvasWidth  ? "absolute left-0" : "relative"
-        )  
-      })
+            window.outerWidth < canvasWidth ? "absolute left-0" : "relative"
+        )
+    })
 
     return <div className={isWindowSmall4Image}>
         {children}
