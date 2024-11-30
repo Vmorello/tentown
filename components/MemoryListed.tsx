5fe40c6d9e0ba23d203bbae0a71487934e486367
vmorello <victorio.morello@gmail.com>
2024-11-30 11:50:50 +0000

    updated file input on tabs

--- components/MemoryListed.tsx
+++ components/MemoryListed.tsx
@@ -5,7 +5,7 @@ import { AdminOptions } from "./AdminDiaryComponents"
 
 const settingIconSize = 40
 
-export function MemoryListed({ memoryList, showCreative, actingCanvasClick, setPreview,userStorageImages,userMaps,removeRep,setCurrentRepInfo }: {
+export function MemoryListed({ memoryList, showCreative, actingCanvasClick, setPreview, userStorageImages, userMaps, removeRep, setCurrentRepInfo }: {
   memoryList: representation[]
   showCreative: boolean
   actingCanvasClick: (x: number, y: number) => void
@@ -37,8 +37,9 @@ export function MemoryListed({ memoryList, showCreative, actingCanvasClick, setP
     if (!rep.hidden || showCreative) {
       return <div className=" border-b last:border-b-0">
         <div className="flex items-center space-x-3 p-3" onClick={() => {
-                    onClick(index)
-                    actingCanvasClick(rep.x + rep.width / 2, rep.y + rep.height / 2)}}>
+          onClick(index)
+          actingCanvasClick(rep.x + rep.width / 2, rep.y + rep.height / 2)
+        }}>
           <div className="w-0 h-0 border-l-8 border-r-8 border-t-8 border-transparent border-t-gray-800"></div>
           <div className="bg-gray-100 rounded-full p-2 flex items-center justify-center">
             <img src={get_image(rep.icon)} alt='settings' height={settingIconSize} width={settingIconSize} className="rounded-full" />
@@ -46,14 +47,18 @@ export function MemoryListed({ memoryList, showCreative, actingCanvasClick, setP
           <div className="text-center">{rep.visible_name}</div>
         </div>
         {showCreative && (index === openedIndex) ? <div>
-          <div> Snapshot:</div>
-          <input type="file" accept="image/*" id={`image_input_${rep.id}`} className="w-full min-h-36 border-2 border-red-300"
+          <div> Add new Photo:</div>
+          <div className="relative w-full max-w-md h-52 border-2 border-dotted border-gray-300 rounded-lg bg-gray-100 flex justify-center items-center cursor-pointer">
+            <input type="file" id={`image_input_${rep.id}`} className="absolute inset-0 w-full h-full opacity-0 cursor-pointer" 
             onChange={(event) => {
-              // console.log(`changing preview file to ${event.target.files![0].name}for ${rep} to `)
-              if (event.target.files === null) {return}
+              if (event.target.files === null) { return }
               setPreview({ item: rep, file: event.target.files[0] })
-            }}/>
-          <AdminOptions item={rep}  userStorageImages={userStorageImages} userMaps={userMaps} removeRep={removeRep} currentRepInfo={memoryList} setCurrentRepInfo={setCurrentRepInfo} />
+            }} />
+            <label htmlFor={`image_input_${rep.id}`} className="text-gray-500 text-lg pointer-events-none">
+              Drag image here or Click to select
+            </label>
+          </div>
+          <AdminOptions item={rep} userStorageImages={userStorageImages} userMaps={userMaps} removeRep={removeRep} currentRepInfo={memoryList} setCurrentRepInfo={setCurrentRepInfo} />
         </div>
           : <></>}
       </div>
