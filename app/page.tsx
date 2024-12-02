a0579b8cf8e3f252646ac04914e073af8270bf91
vmorello <victorio.morello@gmail.com>
2024-12-02 20:14:53 +0000

    removing add images

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default function Index() {
             Let's get you Map-Making
           </h2>
 
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
 
             <Link href="/new/map"
               className="relative flex flex-col group rounded-lg border p-6 hover:border-foreground gap-2">
@@ -27,15 +26,7 @@ export default function Index() {
             </Link>
 
 
-            <Link href="/image"
-              className=" relative flex flex-col group rounded-lg border p-6 hover:border-foreground gap-2">
-              <h3 className="font-bold mb-2  min-h-[40px] lg:min-h-[60px]">
-                Add Images 🪄
-              </h3>
 
-            </Link>
-
-          </div>
         </div>
 
         <div>
